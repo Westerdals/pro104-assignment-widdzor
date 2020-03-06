7ae95a0d2e8fc7412feee3378acbc1fc50d0ad6c
widdzor <markus.somhovd@gmail.com>
2020-03-06 15:23:00 +0000

    implement add task feature

---
+++ addTask.js
@@ -0,0 +1,17 @@
+function addTask(event)
+{
+	event.preventDefault();
+
+	const task = document.querySelector("[name='task']").value;
+
+	const taskList = JSON.parse(window.localStorage.getItem("tasks")) || [];
+
+	const newTask =
+		{
+			'task': task
+		};
+
+	taskList.push(newTask);
+
+	window.localStorage.setItem('tasks', JSON.stringify(taskList));
+}
