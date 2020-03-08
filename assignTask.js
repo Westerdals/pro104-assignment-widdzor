0ebfad2148372f2701f49602f10a4c6d783b3dc2
Mads Trulssen <mtrulssen@gmail.com>
2020-03-08 22:51:03 +0100

    Updated logs

--- assignTask.js
+++ assignTask.js
@@ -1,21 +1,31 @@
-function assignList(event)
-{
-	event.preventDefault();
+function assignList(){
+	var selectMember =  document.getElementById("selectMember")
+	var selectTask = document.getElementById("selectTask")
+	var i = selectMember.selectedIndex;
+	var j = selectTask.selectedIndex;
+	const assignMember = JSON.parse(window.localStorage.getItem("members")) || [];
+	assignMember[selectMember.options[i].value].assignments.push(selectTask.options[j].text)
+	window.localStorage.setItem('members', JSON.stringify(assignMember));
+	console.log(assignMember)
+}
+      var x = document.getElementById("selectTask");
 
-	const task = document.querySelector("[name='task']").value;
-	const name = document.querySelector("[name='name']").value;
+      const assignTasks = JSON.parse(window.localStorage.getItem(`tasks`)) || [];
+      for(var i = 0; i < assignTasks.length; i++) {
+      	var c = document.createElement("option");
+          c.value = i;
+          c.text = assignTasks[i].task;
+          x.options.add(c, i);
+}
 
-	const assignList = JSON.parse(window.localStorage.getItem("members", "tasks")) || [];
-	console.log(assignListldosaldsada)
 
-	/**  const newTask =
-		{
-			'task': task,
-			'member': name,
-		'assignments': assignments
-		};
+      var y = document.getElementById("selectMember");
 
-	assignList.push(newAssign);
-
-	window.localStorage.setItem('assignment', JSON.stringify(assignList));
-**/}
+      const assignMember = JSON.parse(window.localStorage.getItem(`members`)) || [];
+      for(var i = 0; i < assignMember.length; i++) {
+          var v = document.createElement("option");
+          v.value = i;
+          v.text = assignMember[i].member;
+          y.options.add(v, i);
+          console.log(assignMember[i])
+}
