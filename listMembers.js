70615c0cb9ba98b5a83d19c84c7ab44f42d5783d
Håvard Raknes <havard.raknes@gmail.com>
2020-03-16 00:53:09 +0100

    Styling for confirm button

--- listMembers.js
+++ listMembers.js
@@ -1,24 +1,21 @@
-function listMembers()
-{
+function listMembers() {
 	const memberList = JSON.parse(window.localStorage.getItem("members")) || [];
 
 	const list = document.getElementById("member_list");
 
-	while(list.firstChild)
-	{
+	while (list.firstChild) {
 		list.removeChild(list.firstChild);
 	}
 
-	for(const i in memberList)
-	{
+	for (const i in memberList) {
 		const newMember = document.createElement("div");
 		newMember.className = "list_items list_items_member";
 		list.appendChild(newMember);
 		newMember.innerHTML = `${memberList[i].member}`;
 
 		newMember.addEventListener("click", selectMember);
-		newMember.addEventListener("mouseover", onHover);
-		newMember.addEventListener("mouseout", onLeave);
+		// newMember.addEventListener("mouseover", onHover);
+		// newMember.addEventListener("mouseout", onLeave);
 	}
 
 }
